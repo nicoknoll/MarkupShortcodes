d6d61d7356a783b3612d00ff6997df31210fa2df
Nico Knoll <mail@nico-knoll.de>
2012-09-16 18:19:13 +0200

    1.01: Release.

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+
+$shortcode = new ProcessShortcodes();
+
+$shortcode->add('login', function($atts){
+	return '<form><h3>'.$atts['content'].'</h3><label>Enter password for "'.$atts['name'].'":</label> <input type="pass"></form>'; 
+});
+
+$content = 'Lorem ipsum dolor [login name="user"]Login[/login]';
+
+echo $shortcode->render($content);
+
+?>
